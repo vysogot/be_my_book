2c69268f6b0b288735d998f5ab3cb1e4bcf539e6
Jakub Godawa <jgodawa@appriss.com>
2019-10-04 15:09:48 +0200

    change host setting

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :be_my_book, BeMyBookWeb.Endpoint,
-  url: [scheme: "https", host: "be-my-book.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "be-my-book", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
